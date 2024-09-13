fe6a02ec085459648c49f4cfa0ba3d1df83fe162
Dreyton Merck <dmerck@email.sc.edu>
2024-09-13 10:48:44 -0400

    adding assingment 5

---
+++ assignments/assignment_5/script.js
@@ -0,0 +1,17 @@
+// count
+
+let counter = 0;
+
+document.getElementById('count').onclick = () => {
+    counter++;
+    document.getElementById('counter').innerHTML = counter;
+};
+
+// Random Image
+
+document.getElementById('random-image').onclick = () => {
+    location.reload();
+}    
+
+// Slider
+
--- exercises/06_js_intro/script.js
+++ exercises/06_js_intro/script.js
@@ -1,22 +1,32 @@
-const doStuff = () => {
-    alert("Hello World");
-}; // if you want to call more than one time
+// const doStuff = () => {
+//     alert("Hello World");
+ // if you want to call more than one time
 
 const myButton = document.getElementById("say-hello");
 // myButton.onclick = doStuff;
 // only put parenthesis if you want to call the function immediately
 
-mybutton.onclick = () => {
-    document.getElementById("message").innerHTML = "Hello Everyone";
-    document.getElementById("stuff").classList.add("special");
-}; // if you want to call only once for this context
+// mybutton.onclick = () => {
+//     document.getElementById("message").innerHTML = "Hello Everyone";
+//     document.getElementById("stuff").classList.add("special");
+// }; // if you want to call only once for this context
 
-document.getElementById("say-goodbye").onclick = () => {
-    document.getElementById("message").innerHTML = "Goodbye everyone";
-    document.getElementById("stuff").classList.remove("special");
+// document.getElementById("say-goodbye").onclick = () => {
+//     document.getElementById("message").innerHTML = "Goodbye everyone";
+//     document.getElementById("stuff").classList.remove("special");
+// };
+
+
+// showing data from an input field
+
+// document.getElementById("txt-first-name").onkeyup = () => {
+//     const textBox = document.getElementById("txt-first-name");
+//     document.getElementById("result").innerHTML = textBox.value;
+// };
+
+document.getElementById("txt-first-name").onkeyup = (event) => {
+    document.getElementById("result").innerHTML = event.target.value;
 };
-
-
 
 
 
