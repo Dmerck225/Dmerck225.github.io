2d0c699a1db22579fbb55f70bd8e5812e95ec625
Dreyton Merck <dmerck@email.sc.edu>
2024-09-15 21:06:24 -0400

    project part three searchbox resolve

--- assignments/assignment_5/script.js
+++ assignments/assignment_5/script.js
@@ -15,3 +15,17 @@ document.getElementById('random-image').onclick = () => {
 
 // Slider
 
+const slider = document.getElementById('myRange');
+const sliderImg = document.getElementById('slider-img');
+
+slider.oninput = function() {
+    const containerWidth = slider.parentElement.offsetWidth; // Get the container's width
+    const imgWidth = sliderImg.offsetWidth; // Get the image's width
+    const maxMove = containerWidth - imgWidth; // Calculate max movement distance
+
+    // Make sure we start moving left from 0 and not from the middle (50%)
+    const moveDistance = (this.value / 100) * maxMove;
+
+    // Update the left position of the image based on the slider's current value
+    sliderImg.style.left = `${moveDistance}px`;
+};
