adc8b54afc9bf51ba8b174bcb550a0dc577a07cd
Dreyton Merck <dmerck@email.sc.edu>
2024-09-17 12:20:34 -0400

    working on exersise 07

---
+++ exercises/07/script.js
@@ -0,0 +1,9 @@
+// When the hamburger is clicked, toggle between showing and hiding the nav items
+
+// document.getElementById("toggle-nav").onclick = () => {
+//     document.getElementById("nav-items").classList.toggle("hidden-small");
+// };
+
+document.querySelector("#toggle-nav").onclick = () => {
+    document.querySelector("#nav-items").classList.toggle("hidden-small");
+};
