42a82916bc4716216beaead2d5cd5a03a0917ec4
Dreyton Merck <dmerck@email.sc.edu>
2024-09-25 16:28:06 -0400

    adding problem statement

--- assignments/assignment_9/script.js
+++ assignments/assignment_9/script.js
@@ -14,8 +14,8 @@ document.getElementById("star-button").onclick = (e) => {
     for (let i = 0; i < count; i++) {
         const star = document.createElement("div");
         star.classList.add("star");
-        star.style.top = `${Math.random() * 90}%`;
-        star.style.left = `${Math.random() * 90}%`;
+        star.style.top = `${Math.random() * 95}%`;
+        star.style.left = `${Math.random() * 95}%`;
         output.append(star);
     }
 }
