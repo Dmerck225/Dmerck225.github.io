1140a26feacd3ade7b1d22d39932681395d77109
Dreyton Merck <dmerck@email.sc.edu>
2024-09-25 12:14:35 -0400

    updating csce190

---
+++ assignments/assignment_9/script.js
@@ -0,0 +1,21 @@
+// Stars
+
+document.getElementById("star-button").onclick = (e) => {
+    const count = document.getElementById("star-input").value;
+    const output = document.getElementById("star-output");
+    
+    document.getElementById("star-error").innerHTML = "";
+
+    if (count == NaN || count < 1) {
+        document.getElementById("star-error").innerHTML = "* Invalid";
+        return;
+    }
+    
+    for (let i = 0; i < count; i++) {
+        const star = document.createElement("div");
+        star.classList.add("star");
+        star.style.top = `${Math.random() * 90}%`;
+        star.style.left = `${Math.random() * 90}%`;
+        output.append(star);
+    }
+}
--- exercises/08_loops/script.js
+++ exercises/08_loops/script.js
@@ -1,9 +1,20 @@
 document.getElementById("btn-loop") .onclick = () => {
     const ul = document.getElementById("numbers");
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 1; i <= 10; i++) {
         const li = document.createElement("li");
-        li.innerHTML = i+1;
+        li.innerHTML = i;
         ul.append(li);
+        li.setAttribute("id", `li-${i}`);
+
+        li.onclick = () => {
+            console.log(`clicked on ${i}`);
+        };
     }
-}
+};
+
+
+
+// attach event after it is created
+// for loop make div relative and stars absolute. manipulate the top and left position. also make 
+// sure to reference where you got the star from.
