6e1b81735f69856b32a9102271f24952d5901b17
Dreyton Merck <dmerck@email.sc.edu>
2024-10-06 15:01:58 -0400

    adding affinity diagram

--- exercises/10_classes/script.js
+++ exercises/10_classes/script.js
@@ -7,6 +7,59 @@ class Dog {
         this.size = size;
         this.pic = pic;
     }
+
+    get item() {  //can just to .item to call function
+        const section = document.createElement("section");
+        section.classList.add("dog");
+
+        //create a header
+        const h3 = document.createElement("h3");
+        h3.innerHTML = this.title;
+        section.append(h3);
+
+        //create my columns
+        const columnsDiv = document.createElement("div");
+        columnsDiv.classList.add("columns");
+        section.append(columnsDiv);
+        const firstColumn = document.createElement("div");
+        columnsDiv.append(firstColumn);
+        const secondColumn = document.createElement("div");
+        columnsDiv.append(secondColumn);
+
+        //create image and add to first column
+        firstColumn.append(this.picture(this.pic));
+
+        //create info data and add to second column
+        secondColumn.classList.add("hidden");
+        secondColumn.append(this.paragraph("Breed", this.breed));
+        secondColumn.append(this.paragraph("Age", this.age));
+        secondColumn.append(this.paragraph("Color", this.color));
+        secondColumn.append(this.paragraph("Size", this.size));
+
+        //create expand / contract arrow
+        const expCon = document.createElement("a");
+        expCon.href = "#";
+        expCon.innerHTML = "&#x25B6;";
+        h3.append(expCon);
+
+        expCon.onclick = () => {
+            secondColumn.classList.toggle("hidden");
+        }
+
+        return section;
+    }
+
+    picture(info){
+        const pic = document.createElement("img");
+        pic.src = "images/" + info;
+        return pic;
+    }
+
+    paragraph(title, info){
+        const p =document.createElement("p");
+        p.innerHTML = `<strong>${title}</strong>: ${info}`;
+        return p;
+    }
 }
 
 const dogs = [];
@@ -17,5 +70,6 @@ dogs.push(new Dog("Sam", "Golden", "Yellow", 1, "med", "golden-retriever.jpg"));
 dogs.push(new Dog("Gerald", "pit bull", "white", 3, "large", "pitt-bull.jpg"));
 
 dogs.forEach((dog) => {
-    console.log(dog);
+    document.getElementById("dog-list").append(dog.item);
 });
+
