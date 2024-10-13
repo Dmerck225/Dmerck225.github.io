291c5147060f5fc792eaaaf551b8c8fec714d4f2
Dreyton Merck <dmerck@email.sc.edu>
2024-10-13 12:59:21 -0400

    json resolve

--- projects/part6/script.js
+++ projects/part6/script.js
@@ -21,3 +21,34 @@ getCards = async () => {
         console.log(error);
     }
 };
+
+showCards = async () => {
+    const data = await getCards();
+    const cards = data.cards;
+
+    cards.forEach((card) => {
+        document.getElementById("cards").append(getCardSection(card));
+    }); 
+};
+
+const getCardSection = (card) => {
+    const container = document.createElement("div");
+    container.classList.add("card-container");
+    document.getElementById("library-container").append(container);
+
+    const content = document.createElement("div");
+    content.classList.add("card-content");
+    container.append(content);
+
+    const title = document.createElement("div");
+    title.classList.add("card-title");
+    content.append(title);
+
+    const h1 = document.createElement("h1");
+    h1.innerHTML = card.name;
+    title.append(h1);
+
+    return container;
+}
+
+showCards();
